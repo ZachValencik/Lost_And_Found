0fcc8ac569dc0770b81e5ad52b86ef2318682921
ZachValencik <56452654+ZachValencik@users.noreply.github.com>
2021-03-15 13:37:14 -0500

    POST,PUT AND DELETE

--- app.js
+++ app.js
@@ -3,10 +3,12 @@
 
 const express = require('express')
 const app = express()
-const bodyparser = require('body-parser')
+const bodyParser = require('body-parser')
 const port = 5000
 const mysql = require('mysql')
 const { json } = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 
 //this will be lashes db soon
@@ -16,7 +18,8 @@ let mysqlConnection = mysql.createConnection({
   host: process.env.host,
   user: process.env.user,
   password: process.env.password,
-  database: process.env.database
+  database: process.env.database,
+  multipleStatements: true
 })
 
 mysqlConnection.connect((err)=>{
@@ -50,11 +53,59 @@ app.get('/location', (req, res) => {
 
 })
 
-app.get('/location/:name', (req, res) => {
-  mysqlConnection.query('Select * from buildings where name = ?',[req.params.name],(err,row,fields)=>{
+app.get('/location/:location_name', (req, res) => {
+  mysqlConnection.query('Select * from buildings where location_name = ?',[req.params.location_name],(err,row,fields)=>{
 
     if(!err)
     res.send(row)
+   // console.log(rows[0].floors)
+    else
+    console.log(err);
+    
+  })
+})
+
+
+//POST AN LOCATION
+app.post('/location', (req, res) => {
+  let emp = req.body;
+  var sql = "SET @location_name = ?;SET @floor_num = ?;SET @room_num = ?; SET @hasBasement = ?; \
+  CALL LocationAdd(@location_name,@floor_num,@room_num,@hasBasement);";
+  mysqlConnection.query(sql, [emp.location_name, emp.floor_num, emp.room_num, emp.hasBasement], (err, rows, fields) => {
+      if (!err)
+          rows.forEach(element => {
+              if(element.constructor == Array)
+              res.send('Inserted location: '+element[0].location_name);
+          });
+      else
+          console.log(err);
+  })
+
+})
+
+//Put Location
+app.put('/location', (req, res) => {
+  let emp = req.body;
+  var sql = "SET @location_name = ?;SET @floor_num = ?;SET @room_num = ?; SET @hasBasement = ?; \
+  CALL LocationEdit(@location_name,@floor_num,@room_num,@hasBasement);";
+  mysqlConnection.query(sql, [emp.location_name, emp.floor_num, emp.room_num, emp.hasBasement], (err, rows, fields) => {
+      if (!err)
+          res.send('Updated Success');
+        
+      else
+          console.log(err);
+  })
+
+})
+
+
+
+//DELETE LOCATION
+app.delete('/location/:location_name', (req, res) => {
+  mysqlConnection.query(' Delete from buildings where location_name = ?',[req.params.location_name],(err,row,fields)=>{
+
+    if(!err)
+    res.send('Deleted location')
    // console.log(rows[0].floors)
     else
     console.log(err);
