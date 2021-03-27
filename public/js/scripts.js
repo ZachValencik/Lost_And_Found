e2f2c0bad3763e8464e65b2c752bb212f6df2d19
ZachValencik <56452654+ZachValencik@users.noreply.github.com>
2021-03-27 00:09:14 -0500

    Update scripts.js
    Removed duplicated code

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,21 +1,12 @@
 $(document).ready(function(){
 
     $("#testButton").click(function(){
-        console.log("works");
 
         $("#test").html("Display DB Info Below..");
 
       }
+
+
+      
     );
   })
-  
-  $(document).ready(function(){
-
-    $("#testButton").click(function(){
-        console.log("works");
-
-        $("#test").html("Display DB Info Below..");
-
-      }
-    );
-  }) 
