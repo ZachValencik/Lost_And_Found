5badb14caa668916ddb4b043d11e56919f92bd91
ZachValencik <56452654+ZachValencik@users.noreply.github.com>
2021-04-03 18:34:53 -0500

    Made an array of categorys
    We will use it to make seperate tables for each category as instructed in #2

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -23,26 +23,34 @@ $(document).ready(function(){
 
             let oStr = `<h2> Lost Items</h2>`;
             oStr += "<table border='1'> ";
-            oStr += `<tr><th>ID</th><th>Item</th><th>Description</th><th>Lost Location</th><th>Room #</th><th>Found By</th><th>Found By Description</th><th>Date Found</th>
-            <th>Claimed By</th><th>Claimed Description</th></tr>`;
+            oStr += `<tr><th>ID</th><th>Category</th><th>Where Found</th><th>Room #</th><th>Date Found</th></tr>`;
             //alert("success");
             console.log(`data:`);
             console.log( data );
+            let categoryArray =[]; // get the category of the items so we can use it to make a table for each category.
+            for(let i =0; i<data.length;i++){
+              categoryArray.push(data[i].item_category);
+            }
+
+            console.log(categoryArray);
+
             for (let i=0; i<data.length; i++){
                 let ti = data[i].item_id;
-                let t = data[i].item_name;
-                let s = data[i].item_desc;
+               // let t = data[i].item_name;
+                let cat = data[i].item_category;
+               // let s = data[i].item_desc;
                 let c = data[i].item_location;
                 let room = data[i].item_room;
-                let foundBy = data[i].found_by;
-                let foundByDesc = data[i].found_by_desc;
+              //  let foundBy = data[i].found_by;
+              //  let foundByDesc = data[i].found_by_desc;
                 let dateFound = data[i].date_found;
                 let claimedBy = data[i].claimed_by;
-                let claimedDesc = data[i].claimed_desc;
-                oStr += `<tr><td>${ti}</td><td>${t}</td><td>${s}</td><td>${c}</td><td>${room}</td><td>${foundBy}</td><td>${foundByDesc}</td><td>${dateFound}</td><td>${claimedBy}</td><td>${claimedDesc}</td>`;
+              //  let claimedDesc = data[i].claimed_desc;
+                if(claimedBy==null){
+                oStr += `<tr><td>${ti}</td><td>${cat}</td><td>${c}</td><td>${room}</td><td>${dateFound}</td>`;
                 //oStr += `<td> <button type="button" class="btn btn-primary" onClick="deleteIt(${ti})">Delete ${ti} </button> </td>`;
                 oStr += `</tr>`;
-
+                }
             }
             oStr += `</table>`;
             //id.innerHTML = oStr;
