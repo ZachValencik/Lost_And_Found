e08551642e0c6a1e9c7ff4a6548443fc86192602
ZachValencik <56452654+ZachValencik@users.noreply.github.com>
2021-04-15 15:42:08 -0500

    Updated Tests
    Updated blank tests of what should be done in mochai chai

--- tests/testLost_And_Found.js
+++ tests/testLost_And_Found.js
@@ -1,7 +1,7 @@
 describe('Lost and found test ', function() {
-    context( "GET", function() {
-      it( "Login", function(){
-  
+    context( "Admin", function() {
+      it( "Post an Lost Item", function(){
+      
           
         
         
@@ -10,6 +10,73 @@ describe('Lost and found test ', function() {
   
   
     });
+
+    it( "Delete a Lost Item", function(){
+      
+          
+        
+        
+  
+      chai.assert.equal(3,3);
+
+
+  });
+
+  it( "Monthly Report of Lost Items ", function(){
+      
+          
+        
+        
+  
+    chai.assert.equal(3,3);
+
+
+});
+    });
+    context( "Non-Admin Users", function() {
+      it( "Login", function(){
+      
+          
+        
+        
+  
+        chai.assert.equal(3,3);
+    
+    
+    });
+      it( "Display Campus Data", function(){
+      
+          
+        
+        
+  
+        chai.assert.equal(3,3);
+  
+  
+    });
+
+    it( "Display Items By Category", function(){
+      
+          
+        
+        
+  
+      chai.assert.equal(3,3);
+
+
+  });
+
+it( "Logout", function(){
+      
+          
+        
+        
+  
+  chai.assert.equal(3,3);
+
+
+});
+
 
     });
     
