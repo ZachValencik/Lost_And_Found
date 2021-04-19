139cb2fac5b27e24339b8153efc960525148a6e8
ZachValencik <56452654+ZachValencik@users.noreply.github.com>
2021-04-19 18:43:35 -0500

    Added Test for #5 in rubric

---
+++ tests/testNode.js
@@ -0,0 +1,21 @@
+chai.should()
+//https://www.chaijs.com/plugins/chai-jquery/
+describe('#5 Testing 8+ Routes', function() {
+
+    context( "Positive Routes", function() {
+      it( "Log In", function(){
+        
+        
+        
+    });
+ 
+   
+
+
+  
+    });
+  
+  
+  
+  
+  });
